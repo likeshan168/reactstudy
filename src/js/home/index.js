80d70725ac03532671c80daa6cf0c3689f618353
likeshan168 <likeshan168@163.com>
2016-10-31 21:09:07 +0800

    redux

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -1,27 +1,45 @@
+// import React from 'react';
+// // import { render } from 'react-dom'
+// import ReactDOM from 'react-dom';
+// // import { Provider } from 'react-redux'
+// import { applyMiddleware, createStore } from 'redux';
+// import createLogger from 'redux-logger';
+// import thunk from 'redux-thunk'
+// import CounterComponent from './components/Counter';
+// import couterReducer from './reducers';
+// const logger = createLogger();
+// /**中间件是有顺序讲究的，logger中间件一定要放在末尾 */
+// const store = createStore(couterReducer, applyMiddleware(thunk, logger));
+// const rootEl = document.getElementById('root');
+
+
+
+// const render = () => ReactDOM.render(
+//     <CounterComponent value={store.getState()}
+//         onInCrement={() => store.dispatch({ type: 'INCREMENT' })}
+//         onDeCrement={() => store.dispatch({ type: 'DECREMENT' })}
+//         />,
+//     rootEl
+// );
+
+
+// store.subscribe(render);
+// render();
+
+
 import React from 'react';
-// import { render } from 'react-dom'
-import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux';
-import createLogger from 'redux-logger';
-import thunk from 'redux-thunk'
-import CounterComponent from './components/Counter';
-import couterReducer from './reducers';
-const logger = createLogger();
-/**中间件是有顺序讲究的，logger中间件一定要放在末尾 */
-const store = createStore(couterReducer, applyMiddleware(thunk, logger));
-const rootEl = document.getElementById('root');
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 
+import App from '../containers/App';
 
+import configureStore from '../store/configureStore';
 
-const render = () => ReactDOM.render(
-    <CounterComponent value={store.getState()}
-        onInCrement={() => store.dispatch({ type: 'INCREMENT' })}
-        onDeCrement={() => store.dispatch({ type: 'DECREMENT' })}
-        />,
-    rootEl
-);
+const store = configureStore();
 
-
-store.subscribe(render);
-render();
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
